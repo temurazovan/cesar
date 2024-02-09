e7690f697500b1a72d87e4f5473ec7305ab707e4
Nadezhda <nadezhdadieslast@gmail.com>
2024-02-09 06:04:54 -0800

    implement encrypt_cesar function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-string encrypt_caesar()
+std::string encrypt_caesar(std::string text, int shift)  {
+    std::string result;
+    for (int i = 0; i < text.length(); i++) {
+        result += text[i] + shift;
+    }
+    return result;
+}
 
 int main() {
-   int b;
+   std::string text;
+   int shift;
+
+   std::cout << "Input the text: ";
+   std::cin >> text;
+   std::cout << std::endl;
+   std::cout << "Input the shift: ";
+   std::cin >> shift;
+
+   std::cout << encrypt_caesar(text, shift);
+
 }
