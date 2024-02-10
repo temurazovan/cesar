c378ccebbdfb03c2b328b60990a4a8970d90dfba
Nadezhda <nadezhdadieslast@gmail.com>
2024-02-10 04:52:47 -0800

    final version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
-std::string encrypt_caesar(std::string text, int shift)  {
+std::string encrypt_caesar(std::string text, int shift) {
     std::string result;
-    for (int i = 0; i < text.length(); i++) {
-        result += text[i] + shift;
+
+    for (char symbol: text) {
+        if (!(isupper(symbol) || islower(symbol))) {
+            result += symbol;
+            continue;
+        }
+
+        char l = isupper(symbol) ? 'A' : 'a';
+        result += char((symbol - l + shift) % 26) + l;
     }
+
     return result;
 }
 
@@ -15,18 +23,10 @@ int main() {
     std::cout << "Input the text: ";
     std::getline(std::cin, text);
 
-    for (char symbol: text) {
-        if (!((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z') || symbol == ' ')) {
-            std::cout << "Incorrect text!";
-            return 0;
-        }
-
-    }
 
     std::cout << std::endl;
     std::cout << "Input the shift: ";
     std::cin >> shift;
-    shift %= 26;
     std::cout << std::endl;
     std::cout << encrypt_caesar(text, shift);
 
